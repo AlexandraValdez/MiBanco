c30a17f1a9fea60eb29d1a90a71d2dac420fad37
unknown <chris.olivosf@gmail.com>
2023-10-26 10:24:58 -0300

    buscador funcionando con agencias

--- mibanco/src/buscador.jsx
+++ mibanco/src/buscador.jsx
@@ -1,40 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Search() {
-  const [searchTerm, setSearchTerm] = useState(""); // Estado para almacenar el término de búsqueda
+  const [searchTerm, setSearchTerm] = useState(""); // Estado para almacenar búsqueda
   const [searchResults, setSearchResults] = useState([]); // Estado para almacenar los resultados de búsqueda
-const [distritos,setDistritos]=useState([]);
-const [agencias, setAgencias] = useState([]); // Estado para almacenar los datos de agencias
+  const [agencias, setAgencias] = useState([]); // Estado para almacenar las agencias
 
-// useEffect(() => {
-//   // Cargar los datos desde el archivo agencias.json
-//   fetch("./agencias.json")
-//     .then((response) => response.json())
-//     .then((data) => {
-//       setAgencias(data);
-//       console.log(distritos)
-//     })
-//     .catch((error) => console.error('Error:', error));
-// }, []);
+  const MIBANCO_API_URL = './agencias.json';
 
+  useEffect(() => {
+    fetch(MIBANCO_API_URL)
+      .then(response => response.json())
+      .then(data => {
+        setAgencias(data);
+      })
+      .catch(error => console.error('Error:', error));
+  }, []);
 
   const handleSearch = (event) => {
     const searchTerm = event.target.value;
     setSearchTerm(searchTerm);
 
+    // Comprobar si agencias se ha cargado correctamente antes de filtrar
+    if (!agencias) {
+      return;
+    }
 
-
-
-    const sampleData = [
-      { id: 1, name: "Lima" },
-      { id: 2, name: "San Miguel" },
-      { id: 3, name: "San Isidro" },
-    ];
-
-    // Filtrar los resultados que coinciden con el término de búsqueda
-    const results = sampleData.filter((item) =>
-      item.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
+    // Filtrar los resultados que coinciden con el término de búsqueda en el campo "distrito"
+    const results = agencias.filter((agencia) => {
+      return (
+        agencia.DISTRITO &&
+        agencia.DISTRITO.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    });
 
     setSearchResults(results);
   };
@@ -43,22 +40,28 @@ const [agencias, setAgencias] = useState([]); // Estado para almacenar los datos
     <div>
       <input
         type="text"
-        placeholder="Buscar..."
+        placeholder="Buscar por distrito..."
         value={searchTerm}
         onChange={handleSearch}
       />
 
-      <ul>
-        {searchResults.length > 0 ? (
-          searchResults.map((result) => (
-            <li key={result.id}>{result.name}</li>
-          ))
-        ) : (
-          <li>No se encontraron resultados</li>
-        )}
-      </ul>
+      {searchTerm !== "" && (
+        <ul>
+          {searchResults.length > 0 ? (
+            searchResults.map((result, index) => (
+              <li key={index}>
+                <p>Distrito: {result.DISTRITO}</p>
+                <p>Dirección: {result.DIRECCION}</p>
+              </li>
+            ))
+          ) : (
+            <li>No se encontraron resultados</li>
+          )}
+        </ul>
+      )}
     </div>
   );
 }
 
 export default Search;
+
